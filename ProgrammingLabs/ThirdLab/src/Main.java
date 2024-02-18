09323bead166656039fd6996ea6b42dfba2d439a
gryde23 <126186165+gryde23@users.noreply.github.com>
2024-02-18 17:43:52 +0300

    task3 laba3

--- ProgrammingLabs/ThirdLab/src/Main.java
+++ ProgrammingLabs/ThirdLab/src/Main.java
@@ -1,4 +1,6 @@
 
+
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,12 +11,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.print("Введите номер задания: ");
+            System.out.print("\nВведите номер задания: ");
             int input = 0;
             if (scanner.hasNextInt()) input = scanner.nextInt();
             switch (input) {
                 case 1 -> task1();
                 case 2 -> task2();
+                case 3 -> task3();
                 default -> {
                     System.out.println("Такого задания нет. Программа завершена.");
                     return;
@@ -53,6 +56,33 @@ public class Main {
             }
         }
         System.out.printf("Максимальное отрицательное число в массиве: %5d\n", maxNeg);
+    }
+
+    public static void bubblesort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void task3() {
+        Random random = new Random();
+        int[][] matrix = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = random.nextInt(20);
+                System.out.printf("%5d", matrix[i][j]);
+            }
+            System.out.print("\n");
+        }
+        for (int[] row : matrix) {
+            bubblesort(row);
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
 
