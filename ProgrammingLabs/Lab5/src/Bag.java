abd40255e6b56e1f10b0993f35aff4b1f6f64d20
gryde23 <126186165+gryde23@users.noreply.github.com>
2024-03-17 15:20:51 +0300

    лаба 5 задание 2

---
+++ ProgrammingLabs/Lab5/src/Bag.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Bag {
+    private final Random random = new Random();
+    private final int capacity;
+    private ArrayList<Object> bag;
+    public Bag(int max) {
+        this.capacity = max;
+        this.bag = new ArrayList<>(capacity);
+    }
+
+    public <T> void addElement(T elem){
+        if (bag.size() >= capacity) {
+            System.out.println("Мешок полон");
+            return;
+        }
+        if (bag.isEmpty()) {
+            bag.add(elem);
+            return;
+        }
+        int index = random.nextInt(bag.size());
+        bag.add(index, elem);
+    }
+    public Object removeElem(){
+        int index = random.nextInt(bag.size());
+        Object buff = bag.get(index);
+        bag.remove(index);
+        return buff;
+    }
+    public Object getElem(){
+        int index = random.nextInt(bag.size());
+        return bag.get(index);
+    }
+    public int getBagSize(){
+        return bag.size();
+    }
+
+    @Override
+    public String toString() {
+        return bag.toString();
+    }
+}
