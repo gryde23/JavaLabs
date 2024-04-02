e8b6bb7d569991af405282387848a860fa8d7c69
gryde23 <126186165+gryde23@users.noreply.github.com>
2024-04-02 22:43:50 +0300

    исследование операций: начало

---
+++ Operations/Lab1/src/Table.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Table {
+
+    private ArrayList<Integer> Storages;
+    private ArrayList<Integer> Consumers;
+    private ArrayList<ArrayList<Integer>> Costs;
+    private ArrayList<ArrayList<Object>> table;
+
+    public Table(ArrayList<Integer> Storages, ArrayList<Integer> Consumers, ArrayList<ArrayList<Integer>> Costs) {
+        this.Consumers = Consumers;
+        this.Storages = Storages;
+        this.Costs = Costs;
+    }
+
+    public int TransportationPrice(){
+        int price = 0;
+        for (int i = 0; i < table.size(); i++){
+            ArrayList<Object> row = table.get(i);
+            ArrayList<Integer> cost = Costs.get(i);
+            for(int j = 0; j < row.size(); j++) {
+                if (row.get(j).equals("-")) continue;
+                price += (int) row.get(j) * cost.get(j);
+            }
+        }
+        return price;
+    }
+
+    public ArrayList<ArrayList<Object>> getTable() {
+        return table;
+    }
+
+    public void NorthWestDegree(){
+        ArrayList<ArrayList<Object>> table = new ArrayList<>();
+        for(int i = 0; i < Storages.size(); i++) {
+            int a = Storages.get(i);
+            ArrayList<Object> row = new ArrayList<>();
+            for (int j = 0; j < Consumers.size(); j++) {
+                int b = Consumers.get(j);
+                if (a > b && b > 0) {
+                    row.add(b);
+                    Consumers.set(j, 0);
+                    Storages.set(i, a - b);
+                    a -= b;
+                } else if (a > 0 && a <= b) {
+                    row.add(a);
+                    Consumers.set(j, b - a);
+                    Storages.set(i, 0);
+                    a -= a;
+                } else row.add("-");
+            }
+            table.add(row);
+        }
+        this.table = table;
+    }
+}
